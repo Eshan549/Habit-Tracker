a27b65252ac0ca72bade7df89c164e7f57edb10a
Eshan549 <eshanahmedneog@gmail.com>
2026-06-17 17:45:19 +0530

    Removed build output from tracking

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_habittracker_HabitTrackerApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.habittracker.HabitTrackerApp"
-)
-public final class _com_habittracker_HabitTrackerApp {
-}
